6c154839510a5882ce5f47122cc3d656797d9b72
JohnMcKenna1992 <john.mckenna1992@gmail.com>
2018-01-29 21:11:06 +0000

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Prototype Algorithm for Fliteup
+
+/* Simulated Inputs:
+    Origin 1: Glasgow
+    Origin 2: Lisbon
+    Max Price: £100
+    Date Min: 01/04/2018
+    Date Max: 15/04/2018
+*/
+
